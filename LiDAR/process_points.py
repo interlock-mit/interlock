3631cd31aafe2976d5d9c61c9c2e54544174c5ee
--replace-all <annieb22@mit.edu>
2020-11-10 10:56:36 -0600

    filtered points in lane

--- LiDAR/process_points.py
+++ LiDAR/process_points.py
@@ -1,26 +1,29 @@
-# from plyfile import PlyData, PlyElement
-# with open('lidar002310.ply', 'rb') as f:
-#     plydata = PlyData.read(f)
-#     print(plydata.elements)
+from plyfile import PlyData, PlyElement
+import numpy as np
+import open3d as o3d
 
-# import numpy as np
-# import open3d
-# def main():
-#     cloud = open3d.read_point_cloud("lidar002310.ply") # Read the point cloud
-#     print(np.asarray(cloud.points))
-#     open3d.draw_geometries([cloud]) # Visualize the point cloud     
+def visualize(filename):
+    cloud = o3d.io.read_point_cloud(filename) # Read the point cloud
+    o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud
 
-# if __name__ == "__main__":
-#     main()
-count = 0
-with open('lidar002310.ply', 'r') as f:
+def in_car_lane(point):
+    x = float(point[0])
+    return x > -0.5 and x < 0.5
+
+filename = 'LiDAR/lidar002310.ply'
+with open(filename, 'r') as f:
+    points_in_lane = []
     while True:
         line = f.readline()
         if not line:
             break
         line = line.split(" ")
-        if len(line) > 4 and line[4] != "0.0":
-            print(line)
-        # elif len(line) < 4:
-        #     print(line)
-        #     count += 1
+        if len(line) > 4 and in_car_lane(line):
+            line_text = " ".join(line)
+            points_in_lane.append(line_text)
+    points_in_lane = np.array(points_in_lane)
+
+# TODO: write numpy array of filtered points to new .ply file
+new_filename = 'LiDAR/filtered-lidar.ply'
+el = PlyElement.describe(points_in_lane, 'name')
+PlyData([el]).write(new_filename)
