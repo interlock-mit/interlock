abe9bb2dfc75cf8fd224af24bf25cbeecc0dfeab
--replace-all <annieb22@mit.edu>
2020-11-11 14:15:44 -0600

    cleaned up process points

--- LiDAR/process_points.py
+++ LiDAR/process_points.py
@@ -10,24 +10,25 @@ def in_car_lane(point):
     x = float(point[0])
     return x > -6 and x < 6
 
-filename = 'LiDAR/lidar002310.ply'
-with open(filename, 'r') as f:
-    points_in_lane = []
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        line = line.strip("\n").split(" ")
-        if len(line) > 4 and in_car_lane(line):
-            line = [float(num) for num in line]
-            points_in_lane.append(line[:3])
+def create_filtered_point_cloud(filename, new_filename):
+    with open(filename, 'r') as f:
+        points_in_lane = []
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line = line.strip("\n").split(" ")
+            if len(line) > 4 and in_car_lane(line):
+                line = [float(num) for num in line]
+                points_in_lane.append(line[:3])
     points_in_lane = np.array(points_in_lane)
-    print(points_in_lane)
-visualize(filename)
-# TODO: write numpy array of filtered points to new .ply file
-new_filename = 'LiDAR/filtered-lidar.ply'
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points_in_lane)
+    o3d.io.write_point_cloud(new_filename, pcd)
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(points_in_lane)
-o3d.io.write_point_cloud(new_filename, pcd)
+filename = 'LiDAR/lidar002310.ply'
+visualize(filename)
+
+new_filename = 'LiDAR/filtered-lidar.ply'
+create_filtered_point_cloud(filename, new_filename)
 visualize(new_filename)
